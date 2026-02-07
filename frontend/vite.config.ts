f8b803d30fa6cd0ce6ebf4631fe62f4281f550a9
Jovani Trejo <jovanitrejo@icloud.com>
2026-02-07 12:28:46 -0600

    Removed github line changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,22 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-<<<<<<< HEAD
       "/api": "http://localhost:3001",
     },
-=======
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-    port: 5173,
-    host: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    minify: "esbuild",
->>>>>>> cc96240 (updates)
   },
 });
